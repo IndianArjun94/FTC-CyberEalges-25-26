594b038416b8becef93022c9e3d453952fc66cf6
chris <chris.tenscok@gmail.com>
2025-08-31 16:48:34 -0500

    arm update-chris

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpALPHA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpALPHA.java
@@ -12,13 +12,18 @@ public class TeleOpALPHA extends OpMode {
     private static DcMotor rightFrontMotor;
     private static DcMotor leftBackMotor;
     private static DcMotor rightBackMotor;
+    private static DcMotor armMotor;
 
     private static double leftFrontSpeed;
     private static double leftBackSpeed;
     private static double rightFrontSpeed;
     private static double rightBackSpeed;
 
-    private final static float DRIVETRAIN_MULTIPLIER = 0.5f;
+    private final static double DRIVETRAIN_MULTIPLIER = 0.5f;
+    private final static int armPos = armMotor.getCurrentPosition();
+    private static boolean isArmLocked = false;
+    private static boolean isArmRising = false;
+    private final static int ARM_MAX = 1;
 
     @Override
     public void init() {
@@ -26,15 +31,20 @@ public class TeleOpALPHA extends OpMode {
         leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
         rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
         leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
 
         rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    //  Update Function that runs after every loop  //
     public static void update() {
         leftFrontMotor.setPower(leftFrontSpeed * DRIVETRAIN_MULTIPLIER);
         leftBackMotor.setPower(leftBackSpeed * DRIVETRAIN_MULTIPLIER);
         rightBackMotor.setPower(rightBackSpeed * DRIVETRAIN_MULTIPLIER);
         rightFrontMotor.setPower(rightFrontSpeed * DRIVETRAIN_MULTIPLIER);
+        isArmLocked = false;
+        isArmRising = false;
     }
 
     @Override
@@ -48,26 +58,52 @@ public class TeleOpALPHA extends OpMode {
         leftBackSpeed = 0;
         rightBackSpeed = 0;
 
-//        ALL CODE GOES HERE!!!!
+//        ALL CODE GOES HERE!!!!        //
 
-//       Rotate
+//       Rotate     //
         leftBackSpeed -= gamepad1_x2;
         leftFrontSpeed -= gamepad1_x2;
         rightFrontSpeed += gamepad1_x2;
         rightBackSpeed += gamepad1_x2;
 
-//       Forward/Backward
+//       Forward/Backward       //
         leftFrontSpeed += gamepad1_y;
         leftBackSpeed += gamepad1_y;
         rightFrontSpeed += gamepad1_y;
         rightBackSpeed += gamepad1_y;
 
-//       Lateral
+//       Lateral        //
         leftFrontSpeed -= gamepad1_x;
         leftBackSpeed += gamepad1_x;
         rightFrontSpeed += gamepad1_x;
         rightBackSpeed -= gamepad1_x;
+//      Arm code        //
+        if (gamepad1.y) {
+            if (!isArmLocked) {
+                armMotor.setTargetPosition(ARM_MAX);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                isArmRising = true;
 
+            }
+            isArmLocked = false;
+        }
+        else if (gamepad1.a) {
+            if (!isArmLocked) {
+                armMotor.setTargetPosition(ARM_MAX);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                isArmRising = false;
+            }
+            isArmLocked = false;
+        }
+        else {
+            armMotor.setTargetPosition(armPos);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(.5);
+            isArmLocked = true;
+            isArmRising = false;
+        }
+        telemetry.addData("Arm Pos: ", armPos);
+        telemetry.update();
         update();
     }
 }
